48ac6c449f0b2abd32df3bc071d324e84e8ad349
Yun Chung <yun.chung@mac.com>
2020-06-12 16:43:54 +0900

    Created review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_12_073531) do
+ActiveRecord::Schema.define(version: 2020_06_12_074225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,4 +29,14 @@ ActiveRecord::Schema.define(version: 2020_06_12_073531) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.bigint "cocktail_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cocktail_id"], name: "index_reviews_on_cocktail_id"
+  end
+
+  add_foreign_key "reviews", "cocktails"
 end
