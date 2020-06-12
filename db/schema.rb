3a160c2a55455c4b7748410c42df36bb3afc61fc
Yun Chung <yun.chung@mac.com>
2020-06-12 16:45:59 +0900

    Created dose model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_12_074225) do
+ActiveRecord::Schema.define(version: 2020_06_12_074515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 2020_06_12_074225) do
     t.string "photo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "doses", force: :cascade do |t|
+    t.text "description"
+    t.bigint "cocktail_id", null: false
+    t.bigint "ingredient_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
+    t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
   end
 
   create_table "ingredients", force: :cascade do |t|
@@ -38,5 +48,7 @@ ActiveRecord::Schema.define(version: 2020_06_12_074225) do
     t.index ["cocktail_id"], name: "index_reviews_on_cocktail_id"
   end
 
+  add_foreign_key "doses", "cocktails"
+  add_foreign_key "doses", "ingredients"
   add_foreign_key "reviews", "cocktails"
 end
