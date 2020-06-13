a010f51c790274030c193112b3d61b4fd0a2e96b
Yun Chung <yun.chung@mac.com>
2020-06-13 15:11:54 +0900

    Completed route and controller for user to create a new dose to a cocktail

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,4 +1,9 @@
 class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # get "cocktails/:id", to: "cocktails#show", as: :cocktail
   resources :cocktails, only: [:index, :show, :new, :create] do
-    resources :doses, only: [:create]
+    resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
 end
