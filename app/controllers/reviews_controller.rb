9ec938d81a6b74257557548303f85dda632f0b8e
Yun Chung <yun.chung@mac.com>
2020-06-13 20:05:21 +0900

    Coded controller for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,25 @@
 class ReviewsController < ApplicationController
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @review = Review.new(review_params)
+    @review.cocktail = @cocktail
+    if @review.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render "cocktails/show"
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to cocktail_path(@review.cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:rating, :comment)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
     resources :reviews, only: [:create]
   end
   resources :doses, only: [:destroy]
+  resources :reviews, only: [:destroy]
 end
