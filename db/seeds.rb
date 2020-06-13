8a339d87ad0435a34daf116d349a38a4de572b93
Yun Chung <yun.chung@mac.com>
2020-06-13 12:55:13 +0900

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,7 @@ url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 user_serialized = open(url).read
 ingredients = JSON.parse(user_serialized)
 ingredients["drinks"].each do |ingredient|
-  i = Ingredient.create(name: ingredient["strIngredient1"].capitalize)
+  i = Ingredient.create(name: ingredient["strIngredient1"].titleize)
   puts "create #{i.name}"
 end
 
