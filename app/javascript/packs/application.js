cfa275ffeb868912b9b7cfcfebca42a283f2c51b
Yun Chung <yun.chung@mac.com>
2020-06-13 17:37:59 +0900

    Second attempt at setting up Select2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,10 @@ require("channels")
 
 import 'bootstrap';
 
+import { initSelect2 } from '../components/init_select2';
+
+initSelect2();
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
