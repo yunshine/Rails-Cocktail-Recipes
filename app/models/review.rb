e3d8df24117b1bee9a5d5245124f8ae6902e5bfe
Yun Chung <yun.chung@mac.com>
2020-06-15 13:40:54 +0900

    Created container for reviews on show page

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :cocktail
-  validates :rating, :comment, presence: true
+  validates :comment, presence: true
   validates :rating, numericality: true
   validates :rating, numericality: { only_integer: true }
   validates :rating, inclusion: { in: 0..5 }
