6fad2719939fed082af29b2848465438621a16c2
Yun Chung <yun.chung@mac.com>
2020-06-16 12:07:25 +0900

    Added search route to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     resources :doses, only: [:create]
     resources :reviews, only: [:create]
   end
+  resources :search, only: [:index]
   resources :doses, only: [:destroy]
   resources :reviews, only: [:destroy]
 end
