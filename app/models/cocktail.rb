75e0c4f329d957a5ec23677bc1564947436d103a
Yun Chung <yun.chung@mac.com>
2020-06-16 11:17:39 +0900

    Added PgSearch to cocktail model

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -5,4 +5,13 @@ class Cocktail < ApplicationRecord
   has_many :ingredients, through: :doses
   validates :name, :description, :photo, presence: true
   validates :name, uniqueness: true
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [ :name ],
+    associated_against: {
+      ingredient: [ :name ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
